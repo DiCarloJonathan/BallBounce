0061734efdaad38ca2b0b1fcde2040b48997f6af
Kyle Sweeney <KyleMDSweeney@gmail.com>
2015-07-22 15:20:14 -0700

    added timer

---
+++ Assets/Score.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	public Text timeText;
+	float timer=0;
+
+	void Update () {
+		timeText.text = "Time Elapsed: " + (int)timer+" Seconds"; 
+		timer += Time.deltaTime;
+	
+	}
+}
