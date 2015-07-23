fcb4ab128ff03f86914930715e5db7f0de7281f5
Kyle Sweeney <KyleMDSweeney@gmail.com>
2015-07-23 12:19:33 -0700

    fixed but speed on side is fast

--- Assets/Scripts/MovePhisics.cs
+++ Assets/Scripts/MovePhisics.cs
@@ -35,49 +35,62 @@ public class MovePhisics : MonoBehaviour {
 	}
 	void ChangeDirections(){
 		if (ballIsStuck == false) {
-			for(int i=0; i< BrickControll.numBricks; i++)
-				if (BrickControll.brick[i] != null)
-					isBrick(transform.position);
-			if (delta.x + transform.position.x >= 9.5 || delta.x + transform.position.x <= -9.5){
+			for (int i=0; i< BrickControll.numBricks; i++)
+				if (BrickControll.brick [i] != null)
+					isBrick (transform.position);
+			if (delta.x + transform.position.x >= 9.5 || delta.x + transform.position.x <= -9.5) {
 				forces.x *= -1;
 
 			}
-			if (delta.y + transform.position.y >= 6.5){
+			if (delta.y + transform.position.y >= 6.5) {
 				forces.y *= -1;
 			
 			}
-			if (delta.y + transform.position.y <= -6.5) {
+			if (delta.y + transform.position.y <= -9) {
 				SpawnBall.reset = true;
 				Destroy (gameObject);
 			}
 			/*if((delta.y + transform.position.y ) <= (paddle.transform.position.y + (paddle.transform.localScale.y * .5f)) && (delta.x + transform.position.x >=  ){
 
 			}
-			else*/ if ((delta.y + transform.position.y - (transform.localScale.y * .5f)) <= (paddle.transform.position.y + (paddle.delta.y + paddle.transform.localScale.y * .5f)) && ((transform.position.x - .5f + delta.x) <=( paddle.transform.position.x + paddle.delta.x + paddle.transform.localScale.x * .5f) && (transform.position.x + .5f + delta.x) >= paddle.transform.position.x + paddle.delta.x - (paddle.transform.localScale.x * .5f))) {
+			else*/
+			if ((delta.y + transform.position.y - (transform.localScale.y * .5f)) <= (paddle.transform.position.y + (paddle.delta.y + paddle.transform.localScale.y * .5f)) &&
+				((transform.position.y - (transform.localScale.y * .5f)) >= (paddle.transform.position.y + (paddle.transform.localScale.y * .5f))) &&
+				((transform.position.x - .5f + delta.x) <= (paddle.transform.position.x + paddle.delta.x + paddle.transform.localScale.x * .5f) && 
+				(transform.position.x + .5f + delta.x) >= paddle.transform.position.x + paddle.delta.x - (paddle.transform.localScale.x * .5f))) {
 
-					float diff = (transform.position.x - paddle.transform.position.x / (paddle.transform.localScale.x /2))/2;
-					if (diff>0)
-						forces.y = 1- diff;
-					else	
-						forces.y = 1+ diff;
-					forces.x = diff ;
-			//	paddleDir.x += (transform.position.x - paddle.transform.position.x)* howSideways;
+				float diff = (transform.position.x - paddle.transform.position.x / (paddle.transform.localScale.x / 2)) / 2;
+				if (diff > 0)
+					forces.y = 1 - diff;
+				else	
+					forces.y = 1 + diff;
+				forces.x = diff;
+				//	paddleDir.x += (transform.position.x - paddle.transform.position.x)* howSideways;
 				//paddleDir.Normalize ();
 				//forces = Vector3.Reflect (forces, paddleDir);
 				forces.Normalize ();
-
-
-			}
-		} else if (paddle.moving == true)
-			forces = paddle.direction / speed;
-		else {
-			forces = Vector3.zero;	
+			} else if ((delta.y + transform.position.y - .5 <= paddle.transform.position.y + paddle.transform.localScale.y * .5) &&
+				(delta.y + transform.position.y + .5 >= paddle.transform.position.y - paddle.transform.localScale.y * .5) &&
+				((delta.x + transform.position.x + .5 >= paddle.delta.x + paddle.transform.position.x - (.5 * paddle.transform.localScale.x)) &&
+				(transform.position.x + .5 <= paddle.transform.position.x - (.5 * paddle.transform.localScale.x))))
+			        
+				forces.x = -paddle.speed;
+		 else if ((delta.y + transform.position.y - .5 <= paddle.transform.position.y + paddle.transform.localScale.y * .5) &&
+			(delta.y + transform.position.y + .5 >= paddle.transform.position.y - paddle.transform.localScale.y * .5) &&
+			((delta.x + transform.position.x - .5 <= paddle.delta.x + paddle.transform.position.x + (.5 * paddle.transform.localScale.x)) &&
+			(transform.position.x - .5 >= paddle.transform.position.x + (.5 * paddle.transform.localScale.x)))) {
+			forces.x = paddle.speed;
+			
 		}
+		} else if (paddle.moving == true && ballIsStuck == true)
+			forces = paddle.direction / speed;
+		else if(ballIsStuck == true)
+			forces = Vector3.zero;	
 	}
 	void isBrick(Vector3 destination){
 		int c1=0,c2=0;
 		for (int i=0; i< BrickControll.numBricks; i++) {
-			Debug.Log ("for check "+i+" brick" + BrickControll.brick [i]);
+			//Debug.Log ("for check "+i+" brick" + BrickControll.brick [i]);
 			if (BrickControll.brick [i] != null) {
 				if ((destination.x + delta.x + .5 >= BrickControll.brick [i].leftBound && destination.x + .5 <= BrickControll.brick [i].leftBound || destination.x + delta.x - .5 <= BrickControll.brick [i].rightBound && destination.x - .5 >= BrickControll.brick [i].rightBound) && destination.y + delta.y + .5 >= BrickControll.brick [i].lowerBound && destination.y + delta.y - .5 <= BrickControll.brick [i].uperBound) {
 					if(c1==0){
@@ -85,7 +98,7 @@ public class MovePhisics : MonoBehaviour {
 						c1++;
 					}
 					hits.Add (i);
-					Debug.Log ("hitl" + i);
+				//	Debug.Log ("hitl" + i);
 				}
 
 	//	else if ((destination.x + delta.x + .5 >= brick.leftBound && destination.x +.5 <= brick.leftBound ) && destination.x -.5>= brick.rightBound && destination.y + delta.y  +.5 >= brick.lowerBound && destination.y + delta.y  -.5 <= brick.uperBound)
@@ -97,20 +110,20 @@ public class MovePhisics : MonoBehaviour {
 					}
 					if (!hits.Contains (i)) {
 						hits.Add (i);
-						Debug.Log ("hitb" + i);
+					//	Debug.Log ("hitb" + i);
 					}
 				}
 			}
 		}
 		c1 = 0;
 		c2= 0;
-		Debug.Log ("End For Check");
+	//	Debug.Log ("End For Check");
 		if (hits != null) {
 			foreach (int hit in hits){
-				Debug.Log("hit check");
+			//	Debug.Log("hit check");
 				if (BrickControll.brick[hit] != null){
 					BrickControll.brick[hit].GotHit ();
-				Debug.Log("damaged :"+hit);
+		//		Debug.Log("damaged :"+hit);
 				}
 			}
 			hits.Clear();
