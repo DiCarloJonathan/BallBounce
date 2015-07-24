5e6e6f3b7ad81505af87f5ad7a2d3d791d06fb65
Kyle Sweeney <KyleMDSweeney@gmail.com>
2015-07-24 12:25:09 -0700

    Started StartScreen

---
+++ Assets/StartScreenController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreenController : MonoBehaviour {
+
+	public GameObject butonHolder;
+	public GameObject levels;
+	public GameObject startScreen;
+
+	void Awake(){
+		Time.timeScale = 0;
+	}
+
+	public void lSBBehavior(){
+		butonHolder.SetActive (false);
+		levels.SetActive (true);
+	}
+	public void StartButton(){
+		startScreen.SetActive (false);
+		Time.timeScale = 1;
+	}
+	void LoadLevel(){
+
+	}
+	public void SetLevel(){
+
+	}
+}
